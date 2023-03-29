dae6b7bfcbc173226bc0bd18eabea77ef3826787
tomoropy <ucchi1910@gmail.com>
2023-03-29 15:56:34 +0900

    とりあえず動く

--- app/src/pages/api/submitForm.ts
+++ app/src/pages/api/submitForm.ts
@@ -1,28 +1,25 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios from "axios";
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body);
-
   if (req.method === "POST") {
-    try {
-      const {
-        scene,
-        date,
-        location,
-        age,
-        height,
-        hobbies,
-        favoriteCelebrity,
-        occupation,
-      } = req.body;
+    const {
+      scene,
+      date,
+      location,
+      age,
+      height,
+      hobbies,
+      favoriteCelebrity,
+      occupation,
+    } = req.body;
 
-      // OpenAI API キーを環境変数から取得します。
-      const apiKey = process.env.OPENAI_API_KEY;
-
-      // 質問を作成します。
-      const prompt = `以下の条件に合った服装の提案をしてください。
+    const prompt = `以下の条件に合った服装の提案をしてください。
 シーン: ${scene}
 日付: ${date}
 場所: ${location}
@@ -33,30 +30,38 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 好きな芸能人: ${favoriteCelebrity}
 職種: ${occupation}`;
 
-      // OpenAI API にリクエストを送信します。
-      const response = await axios.post(
-        "https://api.openai.com/v1/engines/davinci-codex/completions",
-        {
-          prompt,
-          max_tokens: 100,
-          n: 1,
-          stop: null,
-          temperature: 0.7,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${apiKey}`,
+    try {
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: "以下の条件に合った服装の提案をしてください。",
           },
-        }
-      );
+          { role: "user", content: prompt },
+        ],
+        max_tokens: 100,
+        n: 1,
+        temperature: 0.7,
+      });
 
-      const answer = response.data.choices[0].text.trim();
+      const answer = completion.data.choices[0].message?.content;
+
+      console.log(answer);
+
       res.status(200).json({ answer });
-    } catch (error) {
-      // res.status(500).json({ error: error?.message || error?.toString() });
-      console.log("errorが発生した");
-      console.log(error);
+    } catch (error: any) {
+      if (error.response) {
+        console.error(error.response.status, error.response.data);
+        res.status(error.response.status).json(error.response.data);
+      } else {
+        console.error(`Error with OpenAI API request: ${error.message}`);
+        res.status(500).json({
+          error: {
+            message: "An error occurred during your request.",
+          },
+        });
+      }
     }
   } else {
     res.status(405).json({ error: "Method not allowed" });
